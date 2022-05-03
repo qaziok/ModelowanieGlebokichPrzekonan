a5b10f90c5291791c9b614537935097bd7034195
qaziok <72215548+qaziok@users.noreply.github.com>
2022-05-03 17:42:22 +0200

    zależności między słowami

--- model/generate.py
+++ model/generate.py
@@ -4,9 +4,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
 from preprocessing.article import word_preprocess
 
-
 if __name__ == "__main__":
     df_train = pd.read_csv("../resources/data.csv", sep="\t")
+
+    words_per_article = df_train["data"].apply(word_preprocess)
 
     tfidf_vectorizer = TfidfVectorizer(tokenizer=word_preprocess)
 
@@ -16,13 +17,14 @@ if __name__ == "__main__":
 
     sgdc_classifier.fit(tfidf_wm, df_train['label'])
 
-    with open("../model_storage/vectorizer.pkl","wb") as file:
-        pickle.dump(tfidf_vectorizer,file)
+    with open("../model_storage/words_pa.pkl", "wb") as file:
+        pickle.dump(words_per_article, file)
 
-    with open("../model_storage/bow.pkl","wb") as file:
-        pickle.dump(tfidf_wm,file)
+    with open("../model_storage/vectorizer.pkl", "wb") as file:
+        pickle.dump(tfidf_vectorizer, file)
 
-    with open("../model_storage/classifier.pkl","wb") as file:
-        pickle.dump(sgdc_classifier,file)
+    with open("../model_storage/bow.pkl", "wb") as file:
+        pickle.dump(tfidf_wm, file)
 
-
+    with open("../model_storage/classifier.pkl", "wb") as file:
+        pickle.dump(sgdc_classifier, file)
